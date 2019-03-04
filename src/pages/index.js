aebdb272cbdd362b4ab90f0567748373245f4584
srib <peterf1a08@yahoo.com.hk>
2019-03-04 13:53:09 +0800

    update

--- src/pages/index.js
+++ src/pages/index.js
@@ -15,7 +15,7 @@ export default ({ data }) => {
             border-bottom: 1px solid;
           `}
         >
-          ComusLeong's Blogger
+          Srib's Blogger
         </h1>
         <h4>{data.allMarkdownRemark.totalCount} Posts</h4>
         {data.allMarkdownRemark.edges.map(({ node }) => (
